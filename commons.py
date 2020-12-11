592be4a4709efd1e1405e6a4e9bf2e31dbd95777
Albarra Erdanto <albarraerdanto@gmail.com>
2020-12-11 13:01:48 +0700

    change structure

--- commons.py
+++ commons.py
@@ -7,18 +7,18 @@ import torchvision.transforms as transforms
 
 
 def get_model():
-    model = models.densenet121(pretrained=True)
+    model = pt.load("MaskTypeClassifier_90_87.pth", map_location=pt.device("cpu"))
     model.eval()
     return model
 
 
 def transform_image(image_bytes):
-    my_transforms = transforms.Compose([transforms.Resize(255),
-                                        transforms.CenterCrop(224),
-                                        transforms.ToTensor(),
-                                        transforms.Normalize(
-                                            [0.485, 0.456, 0.406],
-                                            [0.229, 0.224, 0.225])])
+    my_transforms = transforms.Compose([
+        transforms.Resize(256),          
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
     image = Image.open(io.BytesIO(image_bytes))
     return my_transforms(image).unsqueeze(0)
 
